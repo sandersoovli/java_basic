9103749f823542abd1ab63a6221d6e41e781136c
sandersoovli <sander.soovali@voco.ee>
2025-09-10 15:16:19 +0300

    week 2 exercises

---
+++ src/week_2/exercise_29.java
@@ -0,0 +1,11 @@
+package week_2;
+
+public class exercise_29 {
+    public static void main(String[] args) {
+        int number = 2;
+        while (number <= 10) {
+            System.out.println(number);
+            number = number + 1;
+        }
+    }
+}
---
+++ src/week_2/exercsie_26.java
@@ -0,0 +1,21 @@
+package week_2;
+
+import java.util.Scanner;
+
+public class exercsie_26 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }
+             sum = sum + read;
+
+            System.out.println("Sum now: " + sum);
+        }
+        System.out.println("Sum in the end: " +  sum);
+    }
+}
